382f1e78366c29013f865a0087f6b89e1ca6c630
yamil351 <leokasper@live.com.ar>
2023-05-04 16:45:11 -0300

    'error-midleware'

---
+++ helpers/errorHandler.js
@@ -0,0 +1,13 @@
+
+export const errorMiddleware = (err, req, res, next) => {
+  if (res.headersSent) {
+    return next(err);
+  }
+  res.status(err.status || 500);
+  res.send({
+    error: {
+      status: err.status || 500,
+      message: err.message,
+    },
+  });
+};
